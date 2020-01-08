1b1e6944e4af09ff7b1c256d917b7879f48224d5
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-01-08 12:51:44 -0300

    feat(persist): Add persistent store

--- src/app.js
+++ src/app.js
@@ -8,23 +8,45 @@
 
 import React from 'react'
 // State management
+import AsyncStorage from '@react-native-community/async-storage'
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
 import reducers from './data-management/reducers'
+import {persistStore, persistReducer} from 'redux-persist'
+import {PersistGate} from 'redux-persist/integration/react'
 // Nav
 import Navigation from './navigation'
 // Middlewares
 import axiosMiddleware from 'redux-axios-middleware'
+import {createLogger} from 'redux-logger'
 // Api Client
 import apiClient from './services/api-client'
 import {applyMiddleware} from 'redux'
 
-const store = createStore(reducers, applyMiddleware(axiosMiddleware(apiClient)))
+const persistConfig = {
+  // Root
+  key: 'root',
+  // Storage Method (React Native)
+  storage: AsyncStorage,
+  // Whitelist (Save Specific Reducers)
+  whitelist: ['data'],
+}
+// Middleware: Redux Persist Persisted Reducer
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+const store = createStore(
+  persistedReducer,
+  applyMiddleware(axiosMiddleware(apiClient), createLogger()),
+)
+
+const persistor = persistStore(store)
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Navigation />
+      <PersistGate loading={null} persistor={persistor}>
+        <Navigation />
+      </PersistGate>
     </Provider>
   )
 }
