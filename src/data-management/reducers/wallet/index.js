b5a0aee4a613b89d4b021e47137b9760c1200777
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-01-08 12:54:01 -0300

    refactor(wallet-reducer): Refactor wallet loading success case

--- src/data-management/reducers/wallet/index.js
+++ src/data-management/reducers/wallet/index.js
@@ -6,16 +6,35 @@ import {
 } from '../../actions/wallet'
 
 const INITIAL_STATE = {
-  wallet: {},
+  wallets: [],
   loading: false,
+  error: false,
 }
 
 export const walletReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case GET_WALLET:
       return {...state, loading: true}
-    case GET_WALLET_SUCCESS:
-      return {...state, loading: false, wallet: action.payload.data.data}
+    case GET_WALLET_SUCCESS: {
+      const walletData = action.payload.data.data
+      const stateToUpdate = state.wallets
+      console.log('aaaaa', stateToUpdate)
+      const alreadyExists = stateToUpdate.find(
+        ({address}) => address === walletData.address,
+      )
+      console.log('Already exists?', alreadyExists)
+      console.log('Reducer data', walletData)
+
+      return {
+        ...state,
+        loading: false,
+        // wallets: !alreadyExists
+        //   ? stateToUpdate.concat(walletData)
+        //   : stateToUpdate,
+        wallets: stateToUpdate.concat(walletData),
+        error: alreadyExists ? 'Wallet already added' : false,
+      }
+    }
     case GET_WALLET_FAIL:
       return {...state, loading: false, error: 'Error getting wallet info'}
     default:
