35f78db80fcbef223bb3b01dc9cc2f365489a6a6
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-01-10 16:09:29 -0300

    refac(componentization): Add atoms reflection export

---
+++ src/components/atoms/index.js
@@ -0,0 +1,21 @@
+import Button from './button'
+import CardBody from './card-body'
+import CardHeader from './card-header'
+import CardFooter from './card-footer'
+import EmptyListComponent from './empty-list-component'
+import FAB from './floating-action-button'
+import Header from './header'
+import Label from './label'
+import RemoveButton from './remove-button'
+
+export {
+  Button,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  EmptyListComponent,
+  FAB,
+  Header,
+  Label,
+  RemoveButton,
+}
