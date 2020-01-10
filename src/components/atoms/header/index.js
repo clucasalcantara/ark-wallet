b4b834dcc872ed7a2525027f488c01d9db1fddc6
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-01-10 16:08:34 -0300

    refac(componentization): Add header atom

---
+++ src/components/atoms/header/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from '@emotion/native'
+// Assets
+import ArkLogo from '../../../assets/ark_logo.png'
+
+const Wrapper = styled.View({
+  height: 160,
+  justifyContent: 'space-between',
+  alignItems: 'center',
+})
+
+const HeaderText = styled.Text({
+  fontSize: 18,
+  color: 'gray',
+})
+
+const Logo = styled.Image({
+  width: 120,
+  height: 120,
+})
+
+export default ({ title, showLogo }) => (
+  <Wrapper>
+    <HeaderText>{title}</HeaderText>
+    {showLogo && <Logo source={ArkLogo} />}
+  </Wrapper>
+)
