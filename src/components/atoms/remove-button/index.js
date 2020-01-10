944a72c19849b65b72820a3c101da0d904532530
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-01-10 16:09:11 -0300

    refac(componentization): Add remove button atom

---
+++ src/components/atoms/remove-button/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Alert } from 'react-native'
+import styled from '@emotion/native'
+// Assets
+import trashIcon from '../../../assets/trash_icon.png'
+
+const RemoveIcon = styled.Image({
+  height: 20,
+  width: 20,
+})
+
+const RemoveButton = styled.TouchableOpacity({
+  color: 'white',
+  fontSize: 16,
+})
+
+const executeAction = ({ action, type, value }) =>
+  Alert.alert('Warning', `Do you really want to ${type}?`, [
+    {
+      text: 'Confirm',
+      onPress: () => action(value),
+    },
+    {
+      text: 'Cancel',
+      style: 'cancel',
+    },
+  ])
+
+export default ({ action, type, value }) => (
+  <RemoveButton onPress={() => executeAction({ action, type, value })}>
+    <RemoveIcon source={trashIcon} />
+  </RemoveButton>
+)
