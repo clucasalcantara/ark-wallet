d1352f2735a9edcaaf5bf500a475f6e596230b6f
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-01-10 16:11:31 -0300

    refac(navigation): Improve navigation scheme

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,22 +1,22 @@
 import React from 'react'
-import {TouchableOpacity, Text, Image} from 'react-native'
+import { TouchableOpacity, Text, Image } from 'react-native'
 import drawerIcon from '../assets/drawer_button.png'
-import {createAppContainer} from 'react-navigation'
-import {createDrawerNavigator} from 'react-navigation-drawer'
-import {createStackNavigator} from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
+import { createDrawerNavigator } from 'react-navigation-drawer'
+import { createStackNavigator } from 'react-navigation-stack'
 // Screens
-import {Home, Welcome, ListWallets} from '../screens'
+import { Home, Welcome, ListWallets } from '../screens'
 
 const WalletsNavigator = createStackNavigator({
   ListWallets: {
     screen: ListWallets,
-    navigationOptions: ({navigation}) => ({
+    navigationOptions: ({ navigation }) => ({
       title: 'My Wallets',
       headerLeft: () => (
         <TouchableOpacity onPress={() => navigation.openDrawer()}>
           <Image
             source={drawerIcon}
-            style={{marginLeft: 10, height: 20, width: 20}}
+            style={{ marginLeft: 10, height: 20, width: 20 }}
           />
         </TouchableOpacity>
       ),
@@ -25,23 +25,23 @@ const WalletsNavigator = createStackNavigator({
 })
 
 const appNavigator = createDrawerNavigator({
-  Welcome: {
-    screen: Welcome,
-    navigationOptions: () => ({
-      drawerIcon: () => null,
-      drawerLabel: () => null,
-    }),
-  },
   Home: {
     screen: Home,
     navigationOptions: () => ({
-      drawerLabel: 'Import Wallet',
+      drawerLabel: 'Home',
     }),
   },
   ListWallets: {
     screen: WalletsNavigator,
     navigationOptions: () => ({
       drawerLabel: 'My Wallets',
+    }),
+  },
+  Welcome: {
+    screen: Welcome,
+    navigationOptions: () => ({
+      drawerIcon: () => null,
+      drawerLabel: () => null,
     }),
   },
 })
